f48a16500626e45357bbeaab14f95a16f62bcce6
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-12-30 04:32:54 -0800

    build_site.sh: Make bugs easier to find
    I have a few bugs in pages—typos really, but the current build_site
    script makes it hard to find them.  Let's fix that.  Uses VT100-type
    escape sequences now, so there's that.

--- build_site.sh
+++ build_site.sh
@@ -4,30 +4,41 @@ BASEDIR=$(dirname "$0")
 # make find not print ./ and ignore anything starting with . or _
 FIND="find . -path . -o -regex .*/[\._].* -prune -o"
 
-cd $BASEDIR
+if [ "$BASEDIR" != "." ]; then
+	cd $BASEDIR
+	dir="$BASEDIR/"
+else
+	dir=""
+fi
 rm -rf _site
 install -d -m 755 _site
 $FIND -type d -exec mkdir "_site/{}" \;
 $FIND -type f -print0 | while read -d $'\0' file; do
+	file="${file#./}"
 	case "$file" in
-		./build_site.sh)
+		build_site.sh)
 			;;
 		*.html)
 			;;
-		./a2cloud/*.md)
+		a2cloud/*.md)
+			echo -en "\rKRAMDOWN $dir$file ... [K"
 			kramdown --template _layouts/a2cloud.html "$file" \
 				> _site/$(dirname "$file")/$(basename -s ".md" "$file").html
 			;;
-		./a2server/*.md)
+		a2server/*.md)
+			echo -en "\rKRAMDOWN $dir$file ... [K"
 			kramdown --template _layouts/a2server.html "$file" \
 				> _site/$(dirname "$file")/$(basename -s ".md" "$file").html
 			;;
-		./rasppleii/*.md)
+		rasppleii/*.md)
+			echo -en "\rKRAMDOWN $dir$file ... [K"
 			kramdown --template _layouts/rasppleii.html "$file" \
 				> _site/$(dirname "$file")/$(basename -s ".md" "$file").html
 			;;
 		*)
-			cp "$file" "_site/$file"
+			echo -en "\rCOPY $dir$file ... [K"
+			cp "$file" "_site/$file" || echo -e "cp returned $?\n"
 			;;
 	esac
+	echo -e "\n[2A"
 done
